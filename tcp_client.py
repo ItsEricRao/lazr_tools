c18ecd176af426dfda219d8c53fc4a7e38543987
itsericrao <EricRaoMINECRAFT@outlook.com>
2024-04-19 20:42:25 +0800

    test tcp server & client infrastructure.

--- tcp_client.py
+++ tcp_client.py
@@ -1,13 +1,33 @@
 import socket
+import threading
+text_type = "utf-8"
 
-HOST = "127.0.0.1"
-PORT = 7000
+server_ip = "127.0.0.1"
+port  = 7000
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST,PORT))
-        text = input("> ")
-        s.sendall(bytes(text, 'utf-8'))
-        data = s.recv(1024)
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect((server_ip,port))
 
-    print(f"[Server] {data!r}")
+#This function gets other clients messages, so any client has access to all messages
+def messages():
+    while True:
+        try:
+            message = client.recv(1024).decode(text_type)
+            print(message)
+        except:
+            print("ERROR")
+            client.close()
+            break
+
+#This function waits for the user input(message)
+def write():
+    while True:
+        text = str(input("> "))
+        client.send(text.encode(text_type))
+
+
+receive_thread = threading.Thread(target=messages)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
---
+++ tcp_server.py
@@ -0,0 +1,47 @@
+import threading
+import socket
+
+text_type = "utf-8"
+host = "127.0.0.1"
+port = 7000
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen(5)
+
+clients = []
+names = []
+
+def broadcast(msg):
+    for i in clients:
+        message = msg.encode(text_type)
+        i.send(message)
+
+def get_message(client_reference):
+    while True:
+        try:
+
+            message = client_reference.recv(1024).decode(text_type)
+            broadcast(message)
+
+        except:
+
+            index = clients.index(client_reference)
+            clients.remove(client_reference)
+            client_reference.close()
+            break
+
+def new_client():
+    while True:
+
+        client_reference,client_info = server.accept()
+
+        print(f"Connected with {client_info}")
+
+        clients.append(client_reference)
+       
+        thread = threading.Thread(target=get_message, args=[client_reference])
+        thread.start()
+
+
+new_client()
